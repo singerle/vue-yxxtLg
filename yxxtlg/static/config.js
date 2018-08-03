b264561905bac170a22ca76a0268ab17741074ac
lixin <1411341896@qq.com>
2018-08-03 09:05:10 +0800

    update gitignore

--- yxxtlg/static/config.js
+++
@@ -1,9 +0,0 @@
-var config = {
-	// url: 'http://192.168.0.24:8086/enroll/'
-	// url: 'http://zsyx.ctbu.edu.cn:80/enroll/'
-	// url:'http://www.yunhuakeji.com:10071/enroll/',
-	// url: 'http://202.202.144.153:8082/enroll/'
-	url: 'http://yx.cqut.edu.cn/enroll/'//理工
-	// url: 'http://192.168.0.56:8086/enroll/'
-	// url: 'http://192.168.0.56:80/enroll/'
-}
